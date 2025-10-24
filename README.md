0d527bbe3302a0925ad08a4043833b25e52e02ea
Fernanda <ferdsleite@gmail.com>
2025-10-24 19:26:09 +0200

    estado revisado

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-## State Management on React
+# State Management on React
 Lessons to practice state management on React.
+
+- Review about useState
+- Management of direct and indirect components
+- Context API
+- Challenge
+
+## Dependencies: 
+- Tabler icons
+
+## Tech Stack
+- Next.js 16.0
+- React
+- TailwindCSS
+- TypeScript
+
+
 
 
 
--- src/pages/revisao/estado.tsx
+++ src/pages/revisao/estado.tsx
@@ -1,5 +1,33 @@
+import Area from "@/components/template/Area";
+import Link from "next/link";
+import { useState } from "react";
+
 export default function Estado() {
+    const [texto, setTexto] = useState('');
+    
+
     return (
-        <div>Oi</div>
+        <div>
+            <div className="p-20">
+                <Area title="Revisão Estado" color="sky">
+                    <input 
+                        type="text" 
+                        className="input" 
+                        value={texto} 
+                        onChange={e => {
+                            setTexto(e.target.value)
+                        }}
+                    />
+                    <span className="ml-20">{texto}</span>
+
+                </Area>
+            </div>
+            <div className="flex justify-end items-end p-6">
+                <Link href="/">
+                    Back to Home
+                </Link>
+            </div>
+        </div>
+
     )
 }
