329ab3f9626e7b82957107a24a947f2998808130
Fernanda <ferdsleite@gmail.com>
2025-10-29 16:09:39 +0100

    Adicionando componentes na loja

--- src/components/loja/Catalogo.tsx
+++ src/components/loja/Catalogo.tsx
@@ -1,9 +1,18 @@
+import produtos from "@/data/constants/produtos";
 import Area from "../template/Area";
+import ProdutoCard from "./ProdutoCard";
 
 export default function Catalogo() {
+    function renderizarProdutos() {
+        return produtos.map(prod => {
+            return <ProdutoCard key={prod.id} produto={prod} />
+        })
+    }
+
     return (
         <Area title="Catalogo" color="purple">
-            Catalogo
+            {renderizarProdutos()}
+            
         </Area>
     )
 }
---
+++ src/components/loja/ProdutoCard.tsx
@@ -0,0 +1,24 @@
+import Produto from "@/data/model/Produto"
+import Image from "next/image"
+
+interface ProdutoCardProps {
+    produto: Produto
+}
+
+export default function ProdutoCard({produto}: ProdutoCardProps) {
+    return (
+        <div className="flex flex-col">
+            <Image
+                src={produto.imagem}
+                alt="imagem do produto"
+                width={300}
+                height={150}
+                className="rounded-md"
+            />
+            <div className="flex justify-between items-center px-4">
+                <span>{produto.nome}</span>
+                <span>{produto.preco}</span>
+            </div>
+        </div>
+    )
+}
